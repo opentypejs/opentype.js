74f61a30639dc4e92bf94e06f800c461785442fa
Frederik De Bleser <frederik@debleser.be>
2014-07-28 15:00:07 +0200

    Add CFF table encoding.

--- src/tables/cff.js
+++ src/tables/cff.js
@@ -5,6 +5,7 @@
 
 'use strict';
 
+var encode = require('../types').encode;
 var encoding = require('../encoding');
 var _glyph = require('../glyph');
 var parse = require('../parse');
@@ -191,35 +192,35 @@ function parseCFFHeader(data, start) {
     return header;
 }
 
+var TOP_DICT_META = [
+    {name: 'version', op: 0, type: 'SID'},
+    {name: 'notice', op: 1, type: 'SID'},
+    {name: 'copyright', op: 1200, type: 'SID'},
+    {name: 'fullName', op: 2, type: 'SID'},
+    {name: 'familyName', op: 3, type: 'SID'},
+    {name: 'weight', op: 4, type: 'SID'},
+    {name: 'isFixedPitch', op: 1201, type: 'number', value: 0},
+    {name: 'italicAngle', op: 1202, type: 'number', value: 0},
+    {name: 'underlinePosition', op: 1203, type: 'number', value: -100},
+    {name: 'underlineThickness', op: 1204, type: 'number', value: 50},
+    {name: 'paintType', op: 1205, type: 'number', value: 0},
+    {name: 'charstringType', op: 1206, type: 'number', value: 2},
+    {name: 'fontMatrix', op: 1207, type: ['real', 'real', 'real', 'real', 'real', 'real'], value: [0.001, 0, 0, 0.001, 0, 0]},
+    {name: 'uniqueId', op: 13, type: 'number'},
+    {name: 'fontBBox', op: 5, type: ['number', 'number', 'number', 'number'], value: [0, 0, 0, 0]},
+    {name: 'strokeWidth', op: 1208, type: 'number', value: 0},
+    {name: 'xuid', op: 14, type: [], value: null},
+    {name: 'charset', op: 15, type: 'offset', value: 0},
+    {name: 'encoding', op: 16, type: 'offset', value: 0},
+    {name: 'charStrings', op: 17, type: 'offset', value: 0},
+    {name: 'private', op: 18, type: ['number', 'offset'], value: [0, 0]}];
+
 // Parse the CFF top dictionary. A CFF table can contain multiple fonts, each with their own top dictionary.
 // The top dictionary contains the essential metadata for the font, together with the private dictionary.
 function parseCFFTopDict(data, strings) {
-    var dict, meta;
-    meta = [
-        {name: 'version', op: 0, type: 'SID'},
-        {name: 'notice', op: 1, type: 'SID'},
-        {name: 'copyright', op: 1200, type: 'SID'},
-        {name: 'fullName', op: 2, type: 'SID'},
-        {name: 'familyName', op: 3, type: 'SID'},
-        {name: 'weight', op: 4, type: 'SID'},
-        {name: 'isFixedPitch', op: 1201, type: 'number', value: 0},
-        {name: 'italicAngle', op: 1202, type: 'number', value: 0},
-        {name: 'underlinePosition', op: 1203, type: 'number', value: -100},
-        {name: 'underlineThickness', op: 1204, type: 'number', value: 50},
-        {name: 'paintType', op: 1205, type: 'number', value: 0},
-        {name: 'charstringType', op: 1206, type: 'number', value: 2},
-        {name: 'fontMatrix', op: 1207, type: ['number', 'number', 'number', 'number'], value: [0.001, 0, 0, 0.001, 0, 0]},
-        {name: 'uniqueId', op: 13, type: 'number'},
-        {name: 'fontBBox', op: 5, type: ['number', 'number', 'number', 'number'], value: [0, 0, 0, 0]},
-        {name: 'strokeWidth', op: 1208, type: 'number', value: 0},
-        {name: 'xuid', op: 14, type: []},
-        {name: 'charset', op: 15, type: 'offset', value: 0},
-        {name: 'encoding', op: 16, type: 'offset', value: 0},
-        {name: 'charStrings', op: 17, type: 'number', value: 0},
-        {name: 'private', op: 18, type: ['number', 'offset'], value: [0, 0]}
-    ];
+    var dict;
     dict = parseCFFDict(data, 0, data.byteLength);
-    return interpretDict(dict, meta, strings);
+    return interpretDict(dict, TOP_DICT_META, strings);
 }
 
 // Parse the CFF private dictionary. We don't fully parse out all the values, only the ones we need.
@@ -600,6 +601,7 @@ function calcCFFSubroutineBias(subrs) {
 function parseCFFTable(data, start, font) {
     var header, nameIndex, topDictIndex, stringIndex, globalSubrIndex, topDict, privateDictOffset, privateDict,
         subrOffset, subrIndex, charString, charStringsIndex, charset, i;
+    font.tables.cff = {};
     header = parseCFFHeader(data, start);
     nameIndex = parseCFFIndex(data, header.endOffset, parse.bytesToString);
     topDictIndex = parseCFFIndex(data, nameIndex.endOffset);
@@ -610,6 +612,7 @@ function parseCFFTable(data, start, font) {
 
     var topDictData = new DataView(new Uint8Array(topDictIndex.objects[0]).buffer);
     topDict = parseCFFTopDict(topDictData, stringIndex.objects);
+    font.tables.cff.topDict = topDict;
 
     privateDictOffset = start + topDict['private'][1];
     privateDict = parseCFFPrivateDict(data, privateDictOffset, topDict['private'][0], stringIndex.objects);
@@ -643,4 +646,223 @@ function parseCFFTable(data, start, font) {
     }
 }
 
+// Encode the CFF header as a list of bytes.
+// This always returns the same value;
+function encodeHeader() {
+    return [
+        1, // Major version 1
+        0, // Minor version 0
+        4, // This is a 4-byte header
+        1  // 1-byte offsets
+    ];
+}
+
+// Encode the list of font names as a list of bytes.
+function encodeNameIndex(fontNames) {
+    var encodedNames = fontNames.map(function (v) { return encode.NAME(v); });
+    return encode.INDEX(encodedNames);
+}
+
+// Custom equals function that can also check lists.
+function equals(a, b) {
+    if (a === b) {
+        return true;
+    } else if (Array.isArray(a) && Array.isArray(b)) {
+        if (a.length !== b.length) {
+            return false;
+        }
+        for (var i = 0; i < a.length; i += 1) {
+            if (!equals(a[i], b[i])) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Encode the global font attributes as a list of bytes.
+function encodeTopDictIndex(m, strings) {
+    var d, i, entry, value;
+    d = [];
+    for (i = 0; i < TOP_DICT_META.length; i += 1) {
+        entry = TOP_DICT_META[i];
+        value = m[entry.name];
+        if (value !== undefined && !equals(value, entry.value)) {
+            // Operand comes before operator.
+            console.log(entry.op, encode.OPERATOR(entry.op), encode.OPERAND(value, entry.type, strings));
+            d = d.concat(encode.OPERAND(value, entry.type, strings));
+            d = d.concat(encode.OPERATOR(entry.op));
+        }
+    }
+    return encode.INDEX([d]);
+}
+
+function encodeStringIndex(strings) {
+    var encodedStrings = strings.map(function (v) { return encode.STRING(v); });
+    return encode.INDEX(encodedStrings);
+}
+
+function encodeGlobalSubrIndex() {
+    // Currently we don't use subroutines.
+    return encode.INDEX([]);
+}
+
+function encodeEncodings() {
+    var d = [];
+    d.push(0); // Encoding format 0 (apart from .notdef).
+    d.push(1); // Encode 1 glyph.
+    d.push(1); // First charset element maps to first glyph.
+    return d;
+}
+
+function encodeCharsets(strings) {
+    var d = [];
+    d.push(0); // Charset format 0
+    d.push(encode.SID('A', strings));
+    return d;
+}
+
+function pathToOps(path, width) {
+    var ops = [], x, y, i, cmd, dx, dy;
+    ops.push(width);
+    x = 0;
+    y = 0;
+    for (i = 0; i < path.commands.length; i += 1) {
+        cmd = path.commands[i];
+        if (cmd.type === 'M') {
+            dx = x - cmd.x;
+            dy = y - cmd.y;
+            ops.push(dx);
+            ops.push(dy);
+            ops.push('rmoveto');
+            x = cmd.x;
+            y = cmd.y;
+        } else if (cmd.type === 'L') {
+            dx = x - cmd.x;
+            dy = y - cmd.y;
+            ops.push(dx);
+            ops.push(dy);
+            ops.push('rlineto');
+            x = cmd.x;
+            y = cmd.y;
+        }
+    }
+    ops.push('endchar');
+    return ops;
+}
+
+var OPS = {
+    'rmoveto': 21,
+    'rlineto': 5,
+    'endchar': 14
+};
+
+function opsToBytes(ops) {
+    var d = [], i, op;
+    for (i = 0; i < ops.length; i += 1) {
+        op = ops[i];
+        if (typeof op === 'string') {
+            op = OPS[op];
+        }
+        d = d.concat(encode.NUMBER(op));
+    }
+    return d;
+}
+
+function encodeCharString(path, width) {
+    var ops = pathToOps(path, width);
+    console.log(ops);
+    return opsToBytes(ops);
+}
+
+function encodeCharStringsIndex() {
+    // Encode two glyphs: .notdef and A.
+    var notdefPath = new path.Path();
+    notdefPath.moveTo(0, 0);
+    notdefPath.lineTo(0, 500);
+    notdefPath.lineTo(300, 500);
+    notdefPath.lineTo(300, 0);
+
+    var aPath = new path.Path();
+    aPath.moveTo(0, 0);
+    aPath.lineTo(150, 500);
+    aPath.lineTo(300, 0);
+    aPath.moveTo(250, 50);
+    aPath.moveTo(150, 450);
+    aPath.moveTo(50, 50);
+
+    var notdefBytes = encodeCharString(notdefPath, 400);
+    var aBytes = encodeCharString(aPath, 400);
+    return encode.INDEX([notdefBytes, aBytes]);
+}
+
+function encodePrivateDictIndex() {
+    return encode.INDEX([]);
+}
+
+function hexDump(bytes) {
+    var hexString = bytes.map(function (v) {
+        var h =  v.toString(16);
+        return h.length === 1 ? '0' + h : h;
+    });
+    return hexString.join(' ').toUpperCase();
+}
+
+function encodeCFF() {
+    var d, strings, attrs, header, nameIndex, topDictIndex, stringIndex,
+        globalSubrIndex, encodings, charsets, charStringsIndex,
+        privateDictIndex;
+    attrs = {
+        version: 'Version 1.0',
+        fullName: 'Custom Glyph Font',
+        familyName: 'Custom',
+        weight: 'Roman',
+        charset: 0,
+        encoding: 0,
+        charstrings: 0,
+        private: [0, 0]
+    };
+
+    strings = [];
+    d = [];
+    header = encodeHeader();
+    nameIndex = encodeNameIndex(['customfont']);
+    globalSubrIndex = encodeGlobalSubrIndex();
+    encodings = encodeEncodings();
+    charsets = encodeCharsets(strings);
+    charStringsIndex = encodeCharStringsIndex();
+    privateDictIndex = encodePrivateDictIndex();
+    topDictIndex = encodeTopDictIndex(attrs, strings);
+
+    // Needs to come at the end, to encode all custom strings used in the font.
+    stringIndex = encodeStringIndex(strings);
+
+    var baseOffset = header.length + nameIndex.length + topDictIndex.length + stringIndex.length + globalSubrIndex.length;
+    attrs.encoding = baseOffset;
+    attrs.charset = attrs.encoding + encodings.length;
+    attrs.charStrings = attrs.charset + charsets.length;
+    // attrs.private[1] = attrs.charStrings + charStringsIndex.length;
+    // Re-encode the Top DICT Index with the correct offsets.
+    topDictIndex = encodeTopDictIndex(attrs, strings);
+
+    console.log('Header            ', hexDump(header));
+    console.log('Name Index        ', hexDump(nameIndex));
+    console.log('Top DICT Index    ', hexDump(topDictIndex));
+    console.log('String Index      ', hexDump(stringIndex));
+    console.log('Global Subr Index ', hexDump(globalSubrIndex));
+    console.log('Encodings         ', hexDump(encodings));
+    console.log('Charsets          ', hexDump(charsets));
+    console.log('CharStrings Index ', hexDump(charStringsIndex));
+    console.log('Private DICT Index', hexDump(privateDictIndex));
+
+
+    return Array.prototype.concat.call(header,
+                                       nameIndex,
+                                       topDictIndex,
+                                       stringIndex);
+}
+
 exports.parse = parseCFFTable;
+exports.encode = encodeCFF;
---
+++ src/types.js
@@ -0,0 +1,218 @@
+// Data types used in the OpenType font file.
+// All OpenType fonts use Motorola-style byte ordering (Big Endian)
+
+'use strict';
+
+var check = require('./check');
+var encoding = require('./encoding');
+
+var LIMIT16 = 32768; // The limit at which a 16-bit number switches signs == 2^15
+var LIMIT32 = 2147483648; // The limit at which a 32-bit number switches signs == 2 ^ 31
+
+var decode = {};
+var encode = {};
+
+// OpenType data types //////////////////////////////////////////////////////
+
+// Convert an 8-bit unsigned integer to a list of 1 byte.
+encode.BYTE = function (v) {
+    check.argument(v >= 0 && v <= 255, 'Byte value should be between 0 and 255.');
+    return [v];
+};
+
+// Convert a 8-bit signed integer to a list of 1 byte.
+encode.CHAR = function (v) {
+    return [v.charCodeAt(0)];
+};
+
+// Convert a 16-bit unsigned integer to a list of 2 bytes.
+encode.USHORT = function (v) {
+    return [(v >> 8) & 0xFF, v & 0xFF];
+};
+
+// Convert a 16-bit signed integer to a list of 2 bytes.
+encode.SHORT = function (v) {
+    // Two's complement
+    if (v >= LIMIT16){
+        v = - ( 2 * LIMIT16 - v);
+    }
+    return [(v >> 8) & 0xFF, v & 0xFF];
+};
+
+// Convert a 24-bit unsigned integer to a list of 3 bytes.
+encode.UINT24 = function (v) {
+    return [(v >> 16) & 0xFF, (v >> 8) & 0xFF, v & 0xFF];
+};
+
+// Convert a 32-bit unsigned integer to a list of 4 bytes.
+encode.ULONG = function (v) {
+    return [(v >> 24) & 0xFF, (v >> 16) & 0xFF, (v >> 8) & 0xFF, v & 0xFF];
+};
+
+// Convert a 32-bit unsigned integer to a list of 4 bytes.
+encode.LONG = function (v) {
+     // Two's complement
+    if (v >= LIMIT32){
+        v = - ( 2 * LIMIT32 - v);
+    }
+    return [(v >> 24) & 0xFF, (v >> 16) & 0xFF, (v >> 8) & 0xFF, v & 0xFF];
+};
+
+// Convert a 4-char tag to a list of 4 bytes.
+encode.TAG = function (v) {
+    check.argument(v.length === 4, 'Tag should be exactly 4 ASCII characters.');
+    return [v.charCodeAt(0),
+            v.charCodeAt(1),
+            v.charCodeAt(2),
+            v.charCodeAt(3)];
+};
+
+// CFF data types ///////////////////////////////////////////////////////////
+
+encode.Card8 = encode.BYTE;
+encode.Card16 = encode.USHORT;
+encode.OffSize = encode.BYTE;
+encode.SID = encode.USHORT;
+
+// Convert a numeric operand or charstring number to a variable-size list of bytes.
+encode.NUMBER = function (v) {
+    var v2, b0, b1;
+    if (v >= -107 && v <= 107) {
+        return [v + 139];
+    } else if (v >= 108 && v <= 1131 ) {
+        v2 = v - 108;
+        b0 = (v2 >> 8) & 0xFF;
+        b1 = v2 - (b0 << 8);
+        return [b0 + 247, b1];
+    } else if (v >= -1131 && v <= -108) {
+        v2 = -v - 108;
+        b0 = (v2 >> 8) & 0xFF;
+        b1 = v2 - (b0 << 8);
+        return [b0 + 251, b1];
+    } else if (v >= -32768 && v <= 32767) {
+        return encode.NUMBER16(v);
+    } else {
+        return encode.NUMBER32(v);
+    }
+};
+
+// Convert a signed number between -32768 and +32767 to a two-byte value.
+// This ensures we always two bytes, but is not the most compact format.
+encode.NUMBER16 = function (v) {
+    return [28, (v >> 8) & 0xFF, v & 0xFF];
+};
+
+// Convert a signed number between -(2^31) and +(2^31-1) to a four-byte value.
+// This is useful if you want to be sure you always use four bytes,
+// at the expense of wasting a few bytes for smaller numbers.
+encode.NUMBER32 = function (v) {
+    return [29, (v >> 24) & 0xFF, (v >> 16) & 0xFF, (v >> 8) & 0xFF, v & 0xFF];
+};
+
+// Convert an ASCII string to a list of bytes.
+encode.NAME = function (v) {
+    var b = [];
+    for (var i = 0; i < v.length; i += 1) {
+        b.push(v.charCodeAt(i));
+    }
+    return b;
+};
+
+encode.STRING = encode.NAME;
+
+// Convert a list of values to a CFF INDEX structure.
+// The values should already be encoded, that is, they should be arrays.
+encode.INDEX = function (l) {
+    var count, offSize, offset, offsets, offsetEncoder, encodedOffset, data, i, v;
+    if (l.length === 0) {
+        return [0, 0];
+    }
+    count = encode.Card16(l.length);
+    offSize = (1 + Math.floor(Math.log(l.length)/Math.log(2)) / 8) | 0;
+    offset = 1;
+    offsets = [];
+    offsetEncoder = [undefined, encode.BYTE, encode.USHORT, encode.UINT24, encode.ULONG][offSize];
+    data = [];
+    offsets.push(1); // First offset is always 1.
+    for (i = 0; i < l.length; i += 1) {
+        v = l[i];
+        Array.prototype.push.apply(data, v);
+        offset += v.length;
+        encodedOffset = offsetEncoder(offset);
+        Array.prototype.push.apply(offsets, encodedOffset);
+    }
+    return Array.prototype.concat(count,
+                           encode.OffSize(offSize),
+                           offsets,
+                           data);
+};
+
+// Convert a string to a String ID (SID).
+// The list of strings is modified in place.
+encode.SID = function (v, strings) {
+    var i;
+    // Is the string in the CFF standard strings?
+    i = encoding.cffStandardStrings.indexOf(v);
+    if (i >= 0) {
+        return i;
+    }
+    // Is the string already in the string index?
+    i = strings.indexOf(v);
+    if (i >= 0) {
+        return i + encoding.cffStandardStrings.length;
+    } else {
+        strings.push(v);
+        return encoding.cffStandardStrings.length + strings.length;
+    }
+};
+
+encode.OPERATOR = function (v) {
+    if (v < 1200) {
+        return [v];
+    } else {
+        return [12, v - 1200];
+    }
+};
+
+encode.OPERAND = function (v, type, strings) {
+    var d, i, sid;
+    d = [];
+    if (Array.isArray(type)) {
+        for (i = 0; i < type.length; i += 1) {
+            check.argument(v.length === type.length, 'Not enough arguments given for type' + type);
+            d = d.concat(encode.OPERAND(v[i], type[i]));
+        }
+    } else {
+        if (type === 'SID') {
+            sid = encode.SID(v, strings);
+            d = d.concat(encode.NUMBER(sid));
+        } else if (type === 'offset') {
+            // We make it easy for ourselves and always encode offsets as
+            // 4 bytes. This makes offset calculation for the top dict easier.
+            d = d.concat(encode.NUMBER32(v));
+        } else {
+            // FIXME Add support for booleans
+            d = d.concat(encode.NUMBER(v));
+        }
+    }
+    return d;
+};
+
+// Utility functions ////////////////////////////////////////////////////////
+
+// Convert a list of fields to bytes.
+// Each field is an object containing name, type and value.
+encode.FIELDS = function (fields) {
+    var d = [];
+    for (var i = 0; i < fields.length; i += 1) {
+        var field = fields[i];
+        var encodingFunction = encode[field.type];
+        check.argument(encodingFunction !== undefined, 'No encoding function for field type ' + field.type);
+        var bytes = encodingFunction(field.value);
+        d = d.concat(bytes);
+    }
+    return d;
+};
+
+exports.decode = decode;
+exports.encode = encode;
