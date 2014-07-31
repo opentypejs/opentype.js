ede25207da9a535ef99d036ec06b78dd9255195d
Frederik De Bleser <frederik@debleser.be>
2014-07-31 11:19:40 +0200

    Hmtx table takes data from glyphs.

--- src/tables/hmtx.js
+++ src/tables/hmtx.js
@@ -23,13 +23,14 @@ function parseHmtxTable(data, start, numMetrics, numGlyphs, glyphs) {
     }
 }
 
-function makeHmtxTable() {
+function makeHmtxTable(glyphs) {
     var t = new table.Table('hmtx', []);
-    var metrics = [{advanceWidth: 600, leftSideBearing: 0}];
-    for (var i = 0; i < metrics.length; i += 1) {
-        var metric = metrics[i];
-        t.fields.push({name: 'advanceWidth_' + i, type: 'USHORT', value: metric.advanceWidth});
-        t.fields.push({name: 'leftSideBearing_' + i, type: 'SHORT', value: metric.leftSideBearing});
+    for (var i = 0; i < glyphs.length; i += 1) {
+        var glyph = glyphs[i];
+        var advanceWidth = glyph.advanceWidth || 0;
+        var leftSideBearing = glyph.leftSideBearing || 0;
+        t.fields.push({name: 'advanceWidth_' + i, type: 'USHORT', value: advanceWidth});
+        t.fields.push({name: 'leftSideBearing_' + i, type: 'SHORT', value: leftSideBearing});
     }
     return t;
 }
