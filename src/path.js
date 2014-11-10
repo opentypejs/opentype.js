ce16c5e208a73e16e53b49e31bf40b05bb669324
Frederik De Bleser <frederik@debleser.be>
2014-11-10 16:23:40 +0100

    Add support for Path.toSVG().
    You can also access the path data using Path.toPathData(). An optional
    argument allows you to specify the number of decimal places.
    
    This fixes #2.

--- src/path.js
+++ src/path.js
@@ -12,23 +12,47 @@ function Path() {
 }
 
 Path.prototype.moveTo = function (x, y) {
-    this.commands.push({type: 'M', x: x, y: y});
+    this.commands.push({
+        type: 'M',
+        x: x,
+        y: y
+    });
 };
 
 Path.prototype.lineTo = function (x, y) {
-    this.commands.push({type: 'L', x: x, y: y});
+    this.commands.push({
+        type: 'L',
+        x: x,
+        y: y
+    });
 };
 
 Path.prototype.curveTo = Path.prototype.bezierCurveTo = function (x1, y1, x2, y2, x, y) {
-    this.commands.push({type: 'C', x1: x1, y1: y1, x2: x2, y2: y2, x: x, y: y});
+    this.commands.push({
+        type: 'C',
+        x1: x1,
+        y1: y1,
+        x2: x2,
+        y2: y2,
+        x: x,
+        y: y
+    });
 };
 
 Path.prototype.quadTo = Path.prototype.quadraticCurveTo = function (x1, y1, x, y) {
-    this.commands.push({type: 'Q', x1: x1, y1: y1, x: x, y: y});
+    this.commands.push({
+        type: 'Q',
+        x1: x1,
+        y1: y1,
+        x: x,
+        y: y
+    });
 };
 
 Path.prototype.close = Path.prototype.closePath = function () {
-    this.commands.push({type: 'Z'});
+    this.commands.push({
+        type: 'Z'
+    });
 };
 
 // Add the given path or list of commands to the commands of this path.
@@ -66,6 +90,72 @@ Path.prototype.draw = function (ctx) {
         ctx.lineWidth = this.strokeWidth;
         ctx.stroke();
     }
+};
+
+// Convert the Path to a string of path data instructions
+// See http://www.w3.org/TR/SVG/paths.html#PathData
+// Parameters:
+// - decimalPlaces: The amount of decimal places for floating-point values (default: 2)
+Path.prototype.toPathData = function (decimalPlaces) {
+    decimalPlaces = decimalPlaces !== undefined ? decimalPlaces : 2;
+
+    function floatToString(v) {
+        if (Math.round(v) === v) {
+            return '' + Math.round(v);
+        } else {
+            return v.toFixed(decimalPlaces);
+        }
+    }
+
+    function packValues() {
+        var s = '';
+        for (var i = 0; i < arguments.length; i += 1) {
+            var v = arguments[i];
+            if (v > 0 && i > 0) {
+                s += ' ';
+            }
+            s += floatToString(v);
+        }
+        return s;
+    }
+
+    var d = '';
+    for (var i = 0; i < this.commands.length; i += 1) {
+        var cmd = this.commands[i];
+        if (cmd.type === 'M') {
+            d += 'M' + packValues(cmd.x, cmd.y);
+        } else if (cmd.type === 'L') {
+            d += 'L' + packValues(cmd.x, cmd.y);
+        } else if (cmd.type === 'C') {
+            d += 'C' + packValues(cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y);
+        } else if (cmd.type === 'Q') {
+            d += 'Q' + packValues(cmd.x1, cmd.y1, cmd.x, cmd.y);
+        } else if (cmd.type === 'Z') {
+            d += 'Z';
+        }
+    }
+    return d;
+};
+
+// Convert the path to a SVG <path> element, as a string.
+// Parameters:
+// - decimalPlaces: The amount of decimal places for floating-point values (default: 2)
+Path.prototype.toSVG = function (decimalPlaces) {
+    var svg = '<path d="';
+    svg += this.toPathData(decimalPlaces);
+    svg += '"';
+    if (this.fill & this.fill !== 'black') {
+        if (this.fill === null) {
+            svg += ' fill="none"';
+        } else {
+            svg += ' fill="' + this.fill + '"';
+        }
+    }
+    if (this.stroke) {
+        svg += ' stroke="' + this.stroke + '" stroke-width="' + this.strokeWidth + '"';
+    }
+    svg += '/>';
+    return svg;
 };
 
 exports.Path = Path;
