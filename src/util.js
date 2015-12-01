e3697c94d0662fb57717392c90dbebed14c85b1d
Frederik De Bleser <frederik@debleser.be>
2015-12-01 12:10:22 +0100

    Add simple utilities for conversion / env checking.

---
+++ src/util.js
@@ -0,0 +1,33 @@
+'use strict';
+
+exports.isBrowser = function() {
+    return typeof window !== 'undefined';
+};
+
+exports.isNode = function() {
+    return typeof window === 'undefined';
+};
+
+exports.nodeBufferToArrayBuffer = function(buffer) {
+    var ab = new ArrayBuffer(buffer.length);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        view[i] = buffer[i];
+    }
+    return ab;
+}
+
+exports.arrayBufferToNodeBuffer = function(ab) {
+    var buffer = new Buffer(ab.byteLength);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        buffer[i] = view[i];
+    }
+    return buffer;
+}
+
+exports.checkArgument = function(expression, message) {
+    if (!expression) {
+        throw message;
+    }
+}
