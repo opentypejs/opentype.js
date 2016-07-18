fe75355403de4ff85cecffac11c23371ef60ecbe
Vildan <vildan3003@gmail.com>
2016-07-18 12:40:59 -0700

    minor changes in format 12 support

--- src/tables/cmap.js
+++ src/tables/cmap.js
@@ -20,20 +20,20 @@ function parseCmapTableFormat12(cmap, p) {
 	cmap.glyphIndexMap = {};
 
 	for (i = 0; i < groupCount; i += 1) {
-		var glyphIndex;
 		var startCharCode = p.parseULong();
 		var endCharCode = p.parseULong();
-		var startGlyphID = p.parseULong();
+		var startGlyphId = p.parseULong();
 
 		for (var c = startCharCode; c <= endCharCode; c += 1) {
-			cmap.glyphIndexMap[c] = startGlyphID;
-			startGlyphID++;
+			cmap.glyphIndexMap[c] = startGlyphId;
+			startGlyphId++;
 		}
 	}
 }
 
 function parseCmapTableFormat4(cmap, p, data, start, offset) {
 	var i;
+	// Length in bytes of the sub-tables.
 	cmap.length = p.parseUShort();
 	cmap.language = p.parseUShort();
 
