37235900896e9c56aa0e51d56cc2229326d48d37
Harbs <harbs@in-tools.com>
2016-10-09 10:45:09 +0300

    Revert "Refactored the code in ScriptList to make it easier to read."
    This reverts commit 9c36f1cd759f608a52b862acf4e585a3c4128ccb.

--- src/table.js
+++ src/table.js
@@ -113,72 +113,32 @@ function Coverage(coverageTable) {
 Coverage.prototype = Object.create(Table.prototype);
 Coverage.prototype.constructor = Coverage;
 
-/**
- * private
- */
-function addFeatureIndex(arr, indexes) {
-    return arr.concat(ushortList('featureIndex', indexes));
-}
-
-/**
- * private
- */
-function getLangSysArr(langSys) {
-    var retVal = [
-        {name: 'lookupOrder', type: 'USHORT', value: 0},
-        {name: 'reqFeatureIndex', type: 'USHORT', value: langSys.reqFeatureIndex}
-    ];
-    return addFeatureIndex(retVal, langSys.featureIndexes);
-}
-
-/**
- * private
- */
-function getLanguageSysRecords(script) {
-    return recordList('langSys', script.langSysRecords, function(langSysRecord, i) {
-        var langSys = langSysRecord.langSys;
-        return [
-            {name: 'langSysTag' + i, type: 'TAG', value: langSysRecord.tag},
-            {name: 'langSys' + i, type: 'TABLE', value: new Table('langSys', getLangSysArr(langSys))}
-        ];
-    });
-}
-
-/**
- * private
- */
-function getScriptTableArr(script) {
-    var defaultLangSys = script.defaultLangSys;
-    var retVal = [{name: 'defaultLangSys', type: 'TABLE', value: new Table('defaultLangSys', getLangSysArr(defaultLangSys))}];
-    return retVal.concat(getLanguageSysRecords(script));
-}
-
-/**
- * private
- */
-function getScriptTable(script) {
-    return new Table('scriptTable', getScriptTableArr(script));
-}
-
-/**
- * @exports opentype.ScriptList
- * @class
- * @param {opentype.Table}
- * @constructor
- * @extends opentype.Table
- */
 function ScriptList(scriptListTable) {
-    var list = recordList('scriptRecord', scriptListTable, function(scriptRecord, i) {
-        var script = scriptRecord.script;
-        var defaultLangSys = script.defaultLangSys;
-        check.assert(!!defaultLangSys, 'Unable to write GSUB: script ' + scriptRecord.tag + ' has no default language system.');
-        return [
-            {name: 'scriptTag' + i, type: 'TAG', value: scriptRecord.tag},
-            {name: 'script' + i, type: 'TABLE', value: getScriptTable(script)}
-        ];
-    });
-
-    Table.call(this, 'scriptListTable', list);
+    Table.call(this, 'scriptListTable',
+        recordList('scriptRecord', scriptListTable, function(scriptRecord, i) {
+            var script = scriptRecord.script;
+            var defaultLangSys = script.defaultLangSys;
+            check.assert(!!defaultLangSys, 'Unable to write GSUB: script ' + scriptRecord.tag + ' has no default language system.');
+            return [
+                {name: 'scriptTag' + i, type: 'TAG', value: scriptRecord.tag},
+                {name: 'script' + i, type: 'TABLE', value: new Table('scriptTable', [
+                    {name: 'defaultLangSys', type: 'TABLE', value: new Table('defaultLangSys', [
+                        {name: 'lookupOrder', type: 'USHORT', value: 0},
+                        {name: 'reqFeatureIndex', type: 'USHORT', value: defaultLangSys.reqFeatureIndex}]
+                        .concat(ushortList('featureIndex', defaultLangSys.featureIndexes)))}
+                    ].concat(recordList('langSys', script.langSysRecords, function(langSysRecord, i) {
+                        var langSys = langSysRecord.langSys;
+                        return [
+                            {name: 'langSysTag' + i, type: 'TAG', value: langSysRecord.tag},
+                            {name: 'langSys' + i, type: 'TABLE', value: new Table('langSys', [
+                                {name: 'lookupOrder', type: 'USHORT', value: 0},
+                                {name: 'reqFeatureIndex', type: 'USHORT', value: langSys.reqFeatureIndex}
+                                ].concat(ushortList('featureIndex', langSys.featureIndexes)))}
+                        ];
+                    })))}
+            ];
+        })
+    );
 }
 ScriptList.prototype = Object.create(Table.prototype);
 ScriptList.prototype.constructor = ScriptList;
