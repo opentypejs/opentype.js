21529e3d11e37ca8d311396b848db4ab11c5076a
Axel Kittenberger <axkibe@gmail.com>
2017-04-05 12:19:20 +0200

    Fixing 2 Bugs, adding getAdvanceWidth (#274)
    * Appending font hinting to README, improving some comments
    
    * disable hinting checkbox if not available
    
    * Better 'this' handling, adding getAdvanceWidth()
    
    * bugfix glyph.points may not be available yet.
    Might need better handling to get points without doing a path yet.
    
    * When skipping blocks care must be taken not to misunderstand inline data as command.
    
    * typos
    
    * adding getAdvanceWidth to README

--- src/hintingtt.js
+++ src/hintingtt.js
@@ -879,6 +879,38 @@ function initTZone(state)
     }
 }
 
+/*
+* Skips the instruction pointer ahead over an IF/ELSE block.
+* handleElse .. if true breaks on mathing ELSE
+*/
+function skip(state, handleElse)
+{
+    var prog = state.prog;
+    var ip = state.ip;
+    var nesting = 1;
+    var ins;
+
+    do {
+        ins = prog[++ip];
+        if (ins === 0x58) // IF
+            nesting++;
+        else if (ins === 0x59) // EIF
+            nesting--;
+        else if (ins === 0x40) // NPUSHB
+            ip += prog[ip + 1] + 1;
+        else if (ins === 0x41) // NPUSHW
+            ip += 2 * prog[ip + 1] + 1;
+        else if (ins >= 0xB0 && ins <= 0xB7) // PUSHB
+            ip += ins - 0xB0 + 1;
+        else if (ins >= 0xB8 && ins <= 0xBF) // PUSHW
+            ip += (ins - 0xB8 + 1) * 2;
+        else if (handleElse && nesting === 1 && ins === 0x1B) // ELSE
+            break;
+    } while (nesting > 0);
+
+    state.ip = ip;
+}
+
 /*----------------------------------------------------------*
 *          And then a lot of instructions...                *
 *----------------------------------------------------------*/
@@ -1207,26 +1239,10 @@ function ELSE(state) {
     //
     // In case the IF was negative the IF[] instruction already
     // skipped forward over the ELSE[]
-    var ip = state.ip;
-    var prog = state.prog;
 
     if (DEBUG) console.log(state.step, 'ELSE[]');
 
-    var nesting = 1;
-    var ins;
-    do {
-        ins = prog[++ip];
-        switch (ins) {
-            case 0x59: // EIF
-                nesting--;
-                break;
-            case 0x58: // IF
-                nesting++;
-                break;
-        }
-    } while (nesting > 0);
-
-    state.ip = ip;
+    skip(state, false);
 }
 
 // JMPR[] JuMP Relative
@@ -1916,38 +1932,18 @@ function EVEN(state) {
 // IF[] IF test
 // 0x58
 function IF(state) {
-    var ip = state.ip;
-    var prog = state.prog;
-
     var test = state.stack.pop();
     var ins;
 
     if (DEBUG) console.log(state.step, 'IF[]', test);
 
     // if test is true it just continues
-    // if not the ip is skiped until matching ELSE or EIF
+    // if not the ip is skipped until matching ELSE or EIF
     if (!test) {
-        // otherwise skip forward until matching else or endif
-        var nesting = 1;
-        do {
-            ins = prog[++ip];
-            switch (ins) {
-                case 0x1B: // ELSE
-                    if (nesting === 1) nesting--;
-                    break;
-                case 0x59: // EIF
-                    nesting--;
-                    break;
-                case 0x58: // IF
-                    nesting++;
-                    break;
-            }
-        } while (nesting > 0);
+        skip(state, true);
 
         if (DEBUG) console.log(state.step, ins === 0x1B ? 'ELSE[]' : 'EIF[]');
     }
-
-    state.ip = ip;
 }
 
 // EIF[] End IF
