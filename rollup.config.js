791385cb3d2a865ddb6a9c4226c3eb5e82116493
Frederik De Bleser <frederik@debleser.be>
2017-05-15 15:45:06 +0200

    Add the license header.

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ var buble = require('rollup-plugin-buble');
 var resolve = require('rollup-plugin-node-resolve');
 var commonjs = require('rollup-plugin-commonjs');
 var uglify = require('rollup-plugin-uglify');
+var license = require('rollup-plugin-license');
 
 var minify = process.env.MINIFY !== undefined;
 
@@ -20,5 +21,8 @@ module.exports =  {
         commonjs(),
         buble(),
         minify ? uglify() : {},
+        license({
+            banner: 'https://opentype.js.org v<%= pkg.version %> | (c) Frederik De Bleser and other contributors | MIT License | Uses tiny-inflate by Devon Govett'
+        }),
     ]
 };
