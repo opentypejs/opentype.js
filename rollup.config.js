fc5d7618336ebe1e31e3a28e0a10d158c56c27df
Frederik De Bleser <frederik@debleser.be>
2017-05-31 18:30:00 +0000

    Update rollup config to avoid Uglify.
    Rollup now just outputs the flattened JavaScript, and the minify happens
    separately from that.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,11 @@
 var buble = require('rollup-plugin-buble');
 var resolve = require('rollup-plugin-node-resolve');
 var commonjs = require('rollup-plugin-commonjs');
-var uglify = require('rollup-plugin-uglify');
 var license = require('rollup-plugin-license');
-
-var minify = process.env.MINIFY !== undefined;
 
 module.exports =  {
     entry: 'src/opentype.js',
-    dest: minify ? 'dist/opentype.min.js' : 'dist/opentype.js',
+    dest: 'dist/opentype.js',
     moduleName: 'opentype',
     format: 'umd',
     sourceMap: true,
@@ -20,7 +17,6 @@ module.exports =  {
         }),
         commonjs(),
         buble(),
-        minify ? uglify() : {},
         license({
             banner: 'https://opentype.js.org v<%= pkg.version %> | (c) Frederik De Bleser and other contributors | MIT License | Uses tiny-inflate by Devon Govett'
         }),
