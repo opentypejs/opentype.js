35c94585f5ea3138a7b273ec1703ab31b7c6fbb4
Frederik De Bleser <frederik@debleser.be>
2017-07-18 14:45:27 +0200

    Merge branch 'fix-alreadyhaskey-safari-error' of https://github.com/neiltron/opentype.js into neiltron-fix-alreadyhaskey-safari-error

--- src/tables/cff.js
+++ src/tables/cff.js
@@ -161,7 +161,7 @@ function entriesToObject(entries) {
             value = values;
         }
 
-        if (o.hasOwnProperty(key)) {
+        if (o.hasOwnProperty(key) && !isNaN(o[key])) {
             throw new Error('Object ' + o + ' already has key ' + key);
         }
 
