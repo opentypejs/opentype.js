600bc4becdde47418aecf9c16bfd933322da4cb5
Connum <connum@gmail.com>
2023-03-02 22:20:18 +0100

    reformat an comment table test data

--- test/tables/cmap.js
+++ test/tables/cmap.js
@@ -7,7 +7,23 @@ describe('tables/cmap.js', function() {
 
     it('can parse a CMAP format 14 table', function() {
         const cmapData =
-            '000E 00000045 00000003 00FE00 000000000000002B0E010000000034000000000E0101000000000000003C000000010022690003000000010082A600000000010082A60002';
+            '000E 00000045 00000003 ' + // format, length, numVarSelectorRecords
+            // varSelector[numVarSelectorRecords]: varSelector, defaultUVSOffset, nonDefaultUVSOffset
+            '00FE00 00000000 0000002B' + 
+            '0E0100 00000034 00000000' +
+            '0E0101 00000000 0000003C' + 
+            // varSelector[0] nonDefaultUVS: numUVSMappings
+            '00000001 ' +
+            // VariationSelector Record: unicodeValue, glyphID
+            '002269 0003 ' +
+            // varSelector[1] defaultUVS: numUnicodeValueRanges
+            '00000001 ' +
+            // UnicodeRange Record: startUnicodeValue, additionalCount
+            '0082A600 ' +
+            // varSelector[2] nonDefaultUVS: numUVSMappings
+            '00000001 ' +
+            // UnicodeRange Record: startUnicodeValue, additionalCount
+            '0082A6 0002';
         const cmap = {};
         const expectedData = {
             '65024': {
