76effaec8848b7f19e2f1569ebb9410e04e47c51
Tao <show_3@163.com>
2024-03-22 18:38:24 +0800

    Update ccmpReplacement.js
    remove file header

--- src/features/ccmp/contextCheck/ccmpReplacement.js
+++ src/features/ccmp/contextCheck/ccmpReplacement.js
@@ -1,13 +1,3 @@
-/*
- * @Author: TonyJR show_3@163.com
- * @Date: 2024-03-19 16:01:40
- * @LastEditors: TonyJR show_3@163.com
- * @LastEditTime: 2024-03-19 19:30:41
- * @FilePath: /opentype.js/src/features/ccmp/contextCheck/ccmpReplacement.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-
-
 function ccmpReplacementStartCheck(contextParams) {
     return contextParams.index === 0 && contextParams.context.length > 1;
 }
