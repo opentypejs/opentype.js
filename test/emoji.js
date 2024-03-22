56d461243cfd80478c26a6d369fe1f2d2586cced
Tao <show_3@163.com>
2024-03-22 18:40:30 +0800

    Update emoji.js
    remove file header

--- test/emoji.js
+++ test/emoji.js
@@ -1,11 +1,3 @@
-/*
- * @Author: TonyJR show_3@163.com
- * @Date: 2024-03-18 18:24:53
- * @LastEditors: TonyJR show_3@163.com
- * @LastEditTime: 2024-03-22 15:01:25
- * @FilePath: /opentype.js/test/emoji.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import assert from 'assert';
 import { parse } from '../src/opentype.js';
 import { readFileSync } from 'fs';
