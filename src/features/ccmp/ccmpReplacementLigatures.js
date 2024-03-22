86d86bdb1d4708cc8bff9e68854695815fc53cb5
Tao <show_3@163.com>
2024-03-22 18:37:51 +0800

    Update ccmpReplacementLigatures.js
    remove file header

--- src/features/ccmp/ccmpReplacementLigatures.js
+++ src/features/ccmp/ccmpReplacementLigatures.js
@@ -1,12 +1,3 @@
-/*
- * @Author: TonyJR show_3@163.com
- * @Date: 2024-03-19 16:19:59
- * @LastEditors: TonyJR show_3@163.com
- * @LastEditTime: 2024-03-21 19:31:38
- * @FilePath: /opentype.js/src/features/ccmp/ccmp.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-
 import { ContextParams } from '../../tokenizer.js';
 import applySubstitution from '../applySubstitution.js';
 
