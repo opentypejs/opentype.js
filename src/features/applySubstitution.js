85db1a713273d5190d0a60d5b74f6c2b58b7643c
Tao <show_3@163.com>
2024-03-22 18:36:54 +0800

    Update applySubstitution.js
    remove file header

--- src/features/applySubstitution.js
+++ src/features/applySubstitution.js
@@ -1,11 +1,3 @@
-/*
- * @Author: TonyJR show_3@163.com
- * @Date: 2024-03-18 18:01:38
- * @LastEditors: TonyJR show_3@163.com
- * @LastEditTime: 2024-03-22 14:40:59
- * @FilePath: /opentype.js/src/features/applySubstitution.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { SubstitutionAction } from './featureQuery.js';
 
 /**
