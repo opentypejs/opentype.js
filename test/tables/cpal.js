6876f07f8af07881a8a93b199d29e3ecb943db4d
Connum <connum@gmail.com>
2024-03-27 02:50:03 +0100

    fix incorrect colorRecordIndices in CPAL test data

--- test/tables/cpal.js
+++ test/tables/cpal.js
@@ -3,13 +3,13 @@ import { hex, unhex } from '../testutil.js';
 import cpal from '../../src/tables/cpal.js';
 
 describe('tables/cpal.js', function() {
-    const data = '00 00 00 02 00 03 00 04 00 00 00 12 00 00 00 01 00 02 ' +
+    const data = '00 00 00 02 00 02 00 04 00 00 00 10 00 00 00 02 ' +
                  '88 66 BB AA 00 11 22 33 12 34 56 78 DE AD BE EF';
     const obj = {
         version: 0,
         numPaletteEntries: 2,
         colorRecords: [0x8866BBAA, 0x00112233, 0x12345678, 0xDEADBEEF],
-        colorRecordIndices: [0, 1, 2],
+        colorRecordIndices: [0, 2],
     };
 
     it('can parse cpal table', function() {
