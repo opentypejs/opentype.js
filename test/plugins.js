cff7316d0d84ed10718c6e16b1a1deda56fb090a
Connum <connum@gmail.com>
2024-04-24 09:32:28 +0200

    add cff1filePlugin test

--- test/plugins.js
+++ test/plugins.js
@@ -1,29 +1,21 @@
 import assert from 'assert';
 import { unhex } from './testutil.js';
-import opentype, { parse, GlyphSet } from '../src/opentype.js';
+import opentype, { parse } from '../src/opentype.js';
 import cff1filePlugin from '../src/plugins/opentypejs.plugin.cff1file.js';
 import { cff1data } from './tables/cff.js';
-import { createMockObject } from './testutil.js';
 
 describe('plugins.js', function() {
     describe(' opentypejs.plugin.cff1file.js', function() {
         it('parses a standalone CFF1 file', function() {
-            // opentype.plugins.push(cff1filePlugin);
-            const logs = [];
-            const originalConsole = global.console;
-            global.console = createMockObject(logs, global.console);
-            global.console = originalConsole;
             assert.throws(() => {
                 parse(unhex(cff1data).buffer);
             }, 'Font doesn\'t contain TrueType, CFF or CFF2 outlines');
             
+            opentype.plugins.push(cff1filePlugin);
             const font = parse(unhex(cff1data).buffer);
 
-            assert.deepEqual(logs, [
-                'Standalone CFF1 files are not supported directly, but you can use the plugin "opentypejs.plugin.cff1file"',
-                'Font is missing the required table "name"',
-            ]);
-            console.log(font);
+            assert.equal(font.tables.cff.topDict.familyName, 'fn');
+            assert.deepEqual(font.glyphs.glyphs[1].name, 'bumps');
         });
     });
 });
