81039226e9a0dc1cd15d123074a56c48cf9f4dcd
Connum <connum@gmail.com>
2024-04-24 14:05:46 +0200

    throw on unsupported standalone CFF1 fomat

--- src/opentype.js
+++ src/opentype.js
@@ -279,7 +279,7 @@ function parseBuffer(buffer, opt={}) {
     ) {
         throw new Error('PostScript/PS1/T1/Adobe Type 1 fonts are not supported directly, but you can use the plugin "opentypejs.plugin.type1"');
     } else if (data.buffer.byteLength > (3 * sizeOf.Card8() + sizeOf.OffSize()) && parse.getByte(data, 0) === 0x01) {
-        console.warn('Standalone CFF1 files are not supported directly, but you can use the plugin "opentypejs.plugin.cff1file"');
+        throw new Error('Standalone CFF1 files are not supported directly, but you can use the plugin "opentypejs.plugin.cff1file"');
     } else {
         throw new Error('Unsupported OpenType signature ' + signature);
     }
