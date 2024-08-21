77edfa381539cf06698b023132cded758320e94d
Forrest O. <forrest+github.com@forresto.com>
2024-08-21 11:55:15 +0300

    Simplify low memory mode check

--- src/glyphset.mjs
+++ src/glyphset.mjs
@@ -66,11 +66,7 @@ if(typeof Symbol !== 'undefined' && Symbol.iterator) {
  */
 GlyphSet.prototype.get = function(index) {
     // this.glyphs[index] is 'undefined' when low memory mode is on. glyph is pushed on request only.
-    if (this.glyphs[index] === undefined) {
-        if (!this.font._push) {
-            // When not in low memory mode, the index is out of bounds
-            return undefined;
-        }
+    if (this.font._push && this.glyphs[index] === undefined) {
         this.font._push(index);
         if (typeof this.glyphs[index] === 'function') {
             this.glyphs[index] = this.glyphs[index]();
